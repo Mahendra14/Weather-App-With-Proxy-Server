f6a73c44a66572044c1ef6d148f444c3ceea1ee8
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-10-22 12:09:00 +0530

    added fetch to the js

--- public/main.js
+++ public/main.js
@@ -3,6 +3,43 @@ const weatherDisplay = document.querySelector('.weather');
 const weatherForm = document.querySelector('#weather-form');
 const cityInput = document.querySelector('#city-input');
 
+const fetchWeather = async (city) => {
+    const url = "";
+    res = await fetch(url);
+    data = await res.json();
+
+    if(data.cod === '404'){
+        alert('City not found');
+        return;
+    }
+
+    const dataToDisplay = {
+        city: data.name,
+        temp: kelvinToFahrentheit(data.main.temp);
+    }
+
+    addWeatherToDom(dataToDisplay);
+
+
+}
+
+
+//to add display elemetns to dom
+const addWeatherToDom = (data) => {
+    weatherDisplay.innerHTML = `
+    <h1>Weather in ${data.city}</h1>
+    <h2>${data.temp} &deg;F</h2>
+    `
+    cityInput.value = '';
+}
+
+
+
+//convert kelvin to fahrenheit
+const kelvinToFahrentheit = (temp) =>{
+    return Math.ceil(((temp - 273.15) * 9) / 5 + 32);
+}
+
 
 weatherForm.addEventListener('submit' , (e) => {
     e.preventDefault();
