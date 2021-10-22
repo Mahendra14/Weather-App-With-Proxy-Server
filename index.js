8629c0c8f38f3fcb6fdc8ff477ffbcaac34cbe31
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-10-22 12:42:17 +0530

    added router to handle it better

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 
-app.get('/api' , (req,res) => {
-    res.json({success:true});
-})
+//to redirect it to routes/index file
+//generally we need to use ./routes/file_name.js as it is index its fine
+app.get("/api",require("./routes"))
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port : ${PORT}`);
