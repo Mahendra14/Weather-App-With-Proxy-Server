91a8132204bc6996ae907e839861b8f00750aa8e
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-10-22 12:37:46 +0530

    wrote a basic route to test its fine

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors =require('cors')
+require('dotenv').config()
 
 const app = express()
 
@@ -8,6 +9,11 @@ app.use(cors());
 
 //setting port
 const PORT = process.env.PORT || 5000;
+
+
+app.get('/api' , (req,res) => {
+    res.json({success:true});
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port : ${PORT}`);
